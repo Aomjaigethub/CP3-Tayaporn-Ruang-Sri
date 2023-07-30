2f6fb82b4fe146ad6b85d5ac90f88c86796a34fe
Aomjaigethub <66425116+Aomjaigethub@users.noreply.github.com>
2023-07-30 08:55:09 +0700

    Create Exercise4_Tayaporn_R.py
    โปรเจคแรกของเรากับภาษา Python 3
    แบบฝึกหัดที่4 แลคเชอร์ 24
    โปรแกรมเก็บคะแนนรายวิชา

---
+++ Exercise4_Tayaporn_R.py
@@ -0,0 +1,15 @@
+#โปรแกรมเก็บคะแนนรายวิชา
+
+FE = float(input('enter your foundation English score here'))
+GB = float(input('enter your general business score here'))
+CS = float(input('enter your computer science score here'))
+CP = float(input('enter your computer programming score here'))
+
+print("------------------------------------")
+print("your score record are as follow:")
+print("------------------------------------")
+print("foundation English:", FE)
+print("General Business:", GB)
+print("Computer Science:", CS)
+print("Computer Programming", CP)
+print("------------------------------------")

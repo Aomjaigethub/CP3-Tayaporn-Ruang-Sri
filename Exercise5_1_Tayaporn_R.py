8019b6ec7e2931db768a06346b35c9764c1ce5cd
Aomjaigethub <66425116+Aomjaigethub@users.noreply.github.com>
2023-07-30 12:06:54 +0700

    Exercise5_1_Tayaporn_R.py

---
+++ Exercise5_1_Tayaporn_R.py
@@ -0,0 +1,16 @@
+#โปรแกรมสำคัญคำนวณเลข
+
+a = int(input("first number"))
+b = int(input("second number"))
+
+add = a+b
+sub = a-b
+divi = a/b
+multi = a*b
+
+print("a+b=",add)
+print("a-b=",sub)
+print("a/b=",divi)
+print("a*b=",multi)
+
+

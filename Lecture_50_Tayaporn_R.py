d4015b13fa7ea49e376c0eedc81ca6377be58cd6
Aomjaigethub <66425116+Aomjaigethub@users.noreply.github.com>
2023-07-31 11:18:40 +0700

    Create Lecture_50_Tayaporn_R.py
    โปรแกรมฟังชั้นเครื่องคิดเลขง่ายๆ

---
+++ Lecture_50_Tayaporn_R.py
@@ -0,0 +1,23 @@
+#simple calculator function
+
+def add(a,b):
+    sum = a+b
+    print("result: ",sum)
+
+def sub(a,b):
+    sum = a-b
+    print("result: ",sum)
+
+def multi(a,b):
+    sum = a*b
+    print("result: ",sum)
+
+def div(a,b):
+    sum = a/b
+    print("result: ",sum)
+
+
+add(2,2)
+sub(8,5)
+multi(5,5)
+div(10,2)
